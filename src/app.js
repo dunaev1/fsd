a6d755fb9a63e6753e93d2c85f09624de1be53f5
Vladimir Dunaev <dunaev666@gmail.com>
2020-04-19 01:06:50 +0900

    radiogroup, toogle drafts

--- src/app.js
+++ src/app.js
@@ -7,4 +7,6 @@ import "./components/textbox/textbox.css";
 import "./components/dropdown/dropdown.scss";
 import "./components/button/button.scss";
 import "./components/checkbox/checkbox.scss"
+import "./components/radiogroup/radiogroup.scss"
+import "./components/toggle/toggle.scss"
 import './main.css'
